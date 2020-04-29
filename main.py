66e40d60c9bf8f9c84cbc3cd339712ec66feb8ae
rmarticedeno <rmarticedeno@gmail.com>
2020-04-29 01:30:37 -0400

    Added gh-pages integration

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ from config import repo_path, after_pull_instructions
 repo = git.Repo( repo_path if repo_path != '' else os.getcwd())
 
 def get_latest_remote_commit(repo):
-    return repo.git.ls_remote("--heads", "origin", 'master').split()[0]
+    return repo.git.ls_remote("--heads", "origin", 'gh-pages').split()[0]
 
 def get_latest_local_commit(repo):
-    return repo.commit('master')
+    return repo.commit('gh-pages')
 
 if get_latest_local_commit(repo) != get_latest_remote_commit(repo):
-    repo.remotes.origin.pull()
+    git = repo.git
+    repo.heads.default.checkout()
+    git.pull('origin', 'default')
+    git.branch('-d','gh-pages')
+    git.checkout('-b', 'gh-pages')
+    git.pull('origin', 'gh-pages')
     if after_pull_instructions:
         fd = open(after_pull_instructions, 'r')
         instructions = fd.readlines()
